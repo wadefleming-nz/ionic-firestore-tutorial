e207b204475ec7e2a4c91e101c4aed00653bd738
Wade Fleming <WFleming@caci.co.uk>
2020-05-10 17:59:43 +0100

    add id param to detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,27 +4,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
-    path: 'detail',
-    loadChildren: () => import('./pages/detail/detail.module').then( m => m.DetailPageModule)
+    path: 'detail/:id',
+    loadChildren: () =>
+      import('./pages/detail/detail.module').then((m) => m.DetailPageModule),
   },
   {
     path: 'create',
-    loadChildren: () => import('./pages/create/create.module').then( m => m.CreatePageModule)
+    loadChildren: () =>
+      import('./pages/create/create.module').then((m) => m.CreatePageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
