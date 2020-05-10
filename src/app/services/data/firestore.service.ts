5f01b2e5cd0b40785c386581e2d8cdef5169db77
Wade Fleming <WFleming@caci.co.uk>
2020-05-10 22:11:05 +0100

    createSong now saves to firestore

--- src/app/services/data/firestore.service.ts
+++ src/app/services/data/firestore.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
-  constructor() {}
+  constructor(public firestore: AngularFirestore) {}
 
   async createSong(song: {
     albumName: string;
     artistName: string;
     songDescription: string;
     songName: string;
-  }) {}
+  }) {
+    const id = this.firestore.createId();
+    return this.firestore.doc(`songList/${id}`).set({ id, ...song });
+  }
 }
