a309d53eb17c55bb3e0580ae63a1e09c6a1e653b
Wade Fleming <WFleming@caci.co.uk>
2020-05-11 22:32:29 +0100

    add prompt for delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FirestoreService } from '../services/data/firestore.service';
 import { Observable } from 'rxjs';
 import { Song } from '../models/song.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,23 @@ import { Song } from '../models/song.model';
 export class HomePage {
   songs$: Observable<Song[]>;
 
-  constructor(public firestoreService: FirestoreService) {
+  constructor(
+    private firestoreService: FirestoreService,
+    private alertController: AlertController
+  ) {
     this.songs$ = this.firestoreService.getAllSongs();
   }
 
-  deleteSong(id: string) {
-    this.firestoreService.deleteSong(id);
+  async deleteSong(id: string, name: string) {
+    const alert = await this.alertController.create({
+      header: 'Delete Song',
+      message: `Are you sure you want to delete '${name}'?`,
+      buttons: [
+        { text: 'Cancel', role: 'cancel', cssClass: 'secondary' },
+        { text: 'OK', handler: () => this.firestoreService.deleteSong(id) },
+      ],
+    });
+
+    await alert.present();
   }
 }
